8fc3eab4c68628e1b3546142bcd745bf48c2a0cb
sandbox259 <saadaibani3@gmail.com>
2025-01-29 11:26:59 +0530

    hashtable, hashmaps

---
+++ Maps/Maps.java
@@ -0,0 +1,18 @@
+package Maps;
+
+import java.util.HashMap;
+
+import java.util.Map;
+
+public class Maps {
+    public static void main(String args[]) {
+        Map<String, Integer> students = new HashMap<>();
+        students.put("Saad", 71);
+        students.put("Kkrishna", 54);
+        students.put("Sharma", 45);
+        students.put("Mishra", 99);
+
+        System.out.println(students.keySet()); // print all keys
+
+    }
+}
