d84e258fbc5ec8791ee3d69420eb7f2f98f2ed83
sandbox259 <saadaibani3@gmail.com>
2025-02-03 13:23:28 +0530

    threads

---
+++ Multithreading/Test1.java
@@ -0,0 +1,18 @@
+package Multithreading;
+
+public class Test1 {
+    public static void main(String[] args) {
+
+        //World world = new World();
+        // world.start();
+
+        for (;;){
+        System.out.println("Hello Word");
+        System.out.print(Thread.currentThread().getName());
+    } 
+
+    }
+
+    
+}
+ 
