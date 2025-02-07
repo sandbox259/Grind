b51eaece4aa02b8fc8c0902b1c0f3fd7cf5b5a8c
sandbox259 <saadaibani3@gmail.com>
2025-02-07 12:15:02 +0530

    nested maps

---
+++ Hashing/NestedMapExample.java
@@ -0,0 +1,42 @@
+package Hashing;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+
+public class NestedMapExample {
+    public static void main (String Args[]){
+        Map<String , Map<String, Integer>> students = new HashMap<>();
+
+
+    Map<String, Integer> aliceGrades = new HashMap<>();
+    aliceGrades.put("Math", 90);
+    aliceGrades.put("Science", 75);
+    aliceGrades.put("English", 78);
+
+    System.out.println("Initial Mapping"+ aliceGrades );
+    System.out.println("Key Set:" + aliceGrades.entrySet());
+    System.out.println(getGrade(students, "Alice", "Math"));
+    System.out.println(getGrade(students, "Bob", "Science"));
+    System.out.println(getGrade(students, "Charlie", "Math"));
+    }
+
+    public static String getGrade(Map<String, Map<String, Integer>> students, String studentName, String subject) {
+        if ( students.containsKey(studentName)){
+            Map<String, Integer> subjects = students.get(studentName);
+            if (subjects.containsKey(subject)){
+                return studentName + "'s grade in " + subject + "is" + subjects.get(subject);
+            }
+            return subject + " not found for " + studentName;
+        }
+        return "Student " + studentName + " not found";
+}
+
+    
+    
+    
+}
+
+
+
